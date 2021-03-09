a82e5bc4dda9ea759b2df577d5875a00e686995f
Mayte <may.eater@gmail.com>
2021-03-09 20:16:34 +0100

    preparing services

---
+++ vue-eurovision/src/utils/constants.js
@@ -0,0 +1,10 @@
+const urlApi = ''
+// TODO: Constantes para las llamadas a la API
+
+const services = {
+    // 'nombre': urlApi + constante + 'llamada api'
+}
+
+export const constants = {
+    services: services
+}
