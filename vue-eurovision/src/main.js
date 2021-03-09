678594bd6d93842c6af5077b1b9f2eec17b084c7
Mayte <may.eater@gmail.com>
2021-03-09 20:15:14 +0100

    i18n pluggin

--- vue-eurovision/src/main.js
+++ vue-eurovision/src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import i18n from './i18n'
+import { BootstrapVue } from 'bootstrap-vue'
+
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
