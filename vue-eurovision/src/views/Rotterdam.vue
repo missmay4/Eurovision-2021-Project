c05fce0696d54f9c8ff0dc0af80ee56310f21f6c
Mayte <may.eater@gmail.com>
2021-03-09 21:17:44 +0100

    rotterdam view changes

--- vue-eurovision/src/views/Rotterdam.vue
+++ vue-eurovision/src/views/Rotterdam.vue
@@ -3,7 +3,7 @@
     <div class="row">
     </div>
     <div class="row">
-      <div class="col">
+      <div class="col-sm-12 col-md-6 col-lg-6">
         <h1 class="mt-4">Rotterdam 2021</h1>
         <img
           class="mx-auto d-block img-fluid mb"
@@ -33,58 +33,55 @@
         <!-- TODO: Logo Rotterdam 2021 -->
         <p class="text-justify">En cuanto al logo, siguiendo el estilo del del año previo, este representa de manera abstracta los colores de las banderas de los países participantes según la ubicación geográfica de sus capitales con respecto a Róterdam, pasando del concepto de "de dónde venimos" al de "adónde vamos".</p>
       </div>
-      <div class="col">
+      <div class="col-sm-12 col-md-6 col-lg-6">
         <h1 class="mt-4 mb-4">Participantes</h1>
         <p class="text-justify">Esta edición cuenta con un total de 40 países participantes. Hungría y Montenegro mantienen su retirada como anunciaran el año anterior, a la que se suma Armenia debido a su situación económica.</p>
         <p class="text-justify">En esta página, podrás encontrar un <b><router-link to="/singers">apartado</router-link></b> de cada participante con su fotografía, nombre, canción y video músical para que puedas escuchar cada canción</p>
-        <div class="row mb-4 mt-4">
-          <div class="col">
-            <img src="../assets/EuroFlags/EuroNL.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroES.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroIT.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroDE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroFR.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroGB.png" alt="" style="max-width: 40px; max-height: 40px">
-          </div>
-        </div>
+
         <p class="text-justify">Cómo país anfitrión tenemos a <b>Países Bajos</b>, país ganador en 2019 y que le otorga el honor de organizar el festival y el paso automático a la final.</p>
         <p class="text-justify">Cómo paises formadores del <b>Big Five</b> tenemos a <b>España</b>, <b>Italia</b>, <b>Francia</b>, <b>Alemania</b> y <b>Reino Unido</b>, considerados así por ser los países que más aportan económicamente a la UER (Unión Europea de Radiodifusión), encargada de retransmitir el concurso. Estos pasan automáticamente a la final.</p>
         <div class="row mb-4 mt-4">
           <div class="col">
-            <img src="../assets/EuroFlags/EuroAL.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroAT.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroHR.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroMK.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroRS.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroCH.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroAU.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroDE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroEE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroFI.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroIS.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroNO.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroSE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroAM.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroAZ.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroBY.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroGE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroMD.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroRU.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroUA.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroBG.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroCY.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroGR.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroMT.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroPT.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroRO.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroSM.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroBE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroCZ.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroIE.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroIL.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroLV.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroLT.png" alt="" style="max-width: 40px; max-height: 40px">
-            <img src="../assets/EuroFlags/EuroPL.png" alt="" style="max-width: 40px; max-height: 40px">
+            <img src="../assets/EuroFlags/EuroNL.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroES.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroIT.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroDE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroFR.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroGB.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroAL.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroAT.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroHR.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroMK.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroRS.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroCH.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroAU.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroDE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroEE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroFI.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroIS.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroNO.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroSE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroAM.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroAZ.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroBY.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroGE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroMD.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroRU.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroUA.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroBG.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroCY.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroGR.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroMT.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroPT.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroRO.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroSM.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroBE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroCZ.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroIE.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroIL.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroLV.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroLT.png" alt="" class="flag">
+            <img src="../assets/EuroFlags/EuroPL.png" alt="" class="flag">
           </div>
         </div>
         <p class="text-justify">Los demás paises participantes (<b>Albania</b>, <b>Australia</b>, <b>Austria</b>, <b>Azerbaiyán</b>, <b>Bélgica</b>, <b>Bielorrusia</b>, <b>Bulgaria</b>, <b>Chipre</b>, <b>Croacia</b>, <b>Dinamarca</b>, <b>Eslovenia</b>, <b>Estonia</b>, <b>Finlandia</b>, <b>Georgia</b>, <b>Grecia</b>, <b>Irlanda</b>, <b>Islandia</b>, <b>Israel</b>, <b>Letonia</b>, <b>Lituania</b>, <b>Macedonia del Norte</b>, <b>Malta</b>, <b>Moldavia</b>, <b>Noruega</b>, <b>Polonia</b>, <b>Portugal</b>, <b>República Checa</b>, <b>Rumania</b>, <b>Rusia</b>, <b>San Marino</b>, <b>Serbia</b>, <b>Suecia</b>, <b>Suiza</b>, <b>Ucrania</b> ) están obligados a participar en una semifinal, que decidirá su paso a la final del concurso. Durante estas semifinales, tanto el país anfitrión como los pertenecientes al Big Five tendrán ocasión de presentar sus canciones al público antes de la gran final.</p>
@@ -96,5 +93,9 @@
 <style scoped>
   h1 {
      color: #224488;
+  }
+  .flag {
+    max-width: 28px; 
+    max-height: 28px
   }
 </style>
