474fb5201e1f47b675979b40a028bbda4583113b
Mayte <may.eater@gmail.com>
2021-03-30 12:21:56 +0200

    semifinals order and final participants

--- db/inserts.sql
+++ db/inserts.sql
@@ -44,7 +44,7 @@ insert into country (code, name) values
 	('RS', 'Serbia'),
 	('SE', 'Suecia'),
 	('CH', 'Suiza'),
-	('UA', 'Ucrania'),
+	('UA', 'Ucrania')
 
 -- Participants Insert 
 
@@ -55,7 +55,6 @@ insert into participant (country_id, name, song, year, song_link, language) valu
 	((select id from country where name = 'Austria'), 'Vicent Bueno', 'Amen', 2021, 'https://youtu.be/1sY76L68rfs', 'Inglés'),
 	((select id from country where name = 'Azerbaiyán'), 'Efendi', 'Mata Hari', 2021, 'https://youtu.be/HSiZmR1c7Q4', 'Inglés y Azerí'),
 	((select id from country where name = 'Bélgica'), 'Hooverphonic', 'The Wrong Place', 2021, 'https://youtu.be/0EQyG1Yjlgw', 'Inglés'),
-	((select id from country where name = 'Bielorrusia'), 'Galasy ZMesta', '', 2021, '', ''),
 	((select id from country where name = 'Bulgaria'), 'Victoria Georgieva', 'Growing Up is Getting Old', 2021, 'https://youtu.be/qMNxCzuEdVM', 'Inglés'),
 	((select id from country where name = 'Chipre'), 'Elena Tsagrinou', 'El Diablo', 2021, 'https://youtu.be/mk4amZlPa4g', 'Inglés y Español'),
 	((select id from country where name = 'Croacia'), 'Albina', 'Tick-tock', 2021, 'https://youtu.be/IHS_2GpNwmA', 'Inglés y Croata'),
@@ -69,7 +68,7 @@ insert into participant (country_id, name, song, year, song_link, language) valu
 	((select id from country where name = 'Grecia'), 'Stefania Liberakakis', 'Last Dance', 2021, 'https://youtu.be/Er06NBWo4bs', 'Inglés'),
 	((select id from country where name = 'Irlanda'), 'Lesley Roy', 'Maps', 2021, 'https://youtu.be/z6ZUBzqPxds', 'Inglés'),
 	((select id from country where name = 'Islandia'), 'Daði & Gagnamagnið', '10 Years', 2021, 'https://youtu.be/ORDK1XQToAY', 'Inglés'),
-	((select id from country where name = 'Israel'), 'Eden Alene', 'Set Me Free', 2021, 'https://youtu.be/opmTs9uvENI', 'Inglés y Hebreo'),
+	((select id from country where name = 'Israel'), 'Eden Alene', 'Set Me Free', 2021, 'https://youtu.be/9nss3FsrgJo', 'Inglés y Hebreo'),
 	((select id from country where name = 'Italia'), 'Måneskin', 'Zitti e buoni', 2021, 'https://youtu.be/9mL6Cmkg2_A', 'Italiano'),
 	((select id from country where name = 'Letonia'), 'Samanta Tīna', 'The Moon is Rising', 2021, 'https://youtu.be/NBaCWHSyDzo', 'Inglés'),
 	((select id from country where name = 'Lituania'), 'The Roop', 'Discoteque', 2021, 'https://youtu.be/0rsUJWSwb0c', 'Inglés'),
@@ -99,44 +98,42 @@ insert into gala (type, date) values
 -- galaParticipant Insert 
 -- 1st Semifinal 18/5/2021
 insert into galaparticipant (gala_id, participant_id, performanceorder) values	
-	(1, 26, ), -- Macedonia
-	(1, 8, ), -- Belarus
-	(1, 25, ), -- Lithuania
-	(1, 39, ), -- Sweden
-	(1, 13, ), -- Slovenia
-	(1, 4, ), -- Australia
-	(1, 20, ), -- Ireland
-	(1, 36, ), -- Russia 
-	(1, 29, ), -- Norway
-	(1, 10, ), -- Cyprus
-	(1, 11, ), -- Croatia
-	(1, 6, ), -- Azerbaijan
-	(1, 27, ), -- Malta
-	(1, 22, ), -- Israel
-	(1, 41, ), -- Ukraine
-	(1, 35, ), -- Romania
-	(1, 7, ) -- Belgium
+	(1, 25, 1), -- Lithuania
+	(1, 13, 2), -- Slovenia
+	(1, 36, 3), -- Russia
+	(1, 39, 4), -- Sweden
+	(1, 4, 5), -- Australia
+	(1, 26, 6), -- Macedonia
+	(1, 20, 7), -- Ireland
+	(1, 10, 8), -- Cyprus
+	(1, 29, 9), -- Norway
+	(1, 11, 10), -- Croatia
+	(1, 7, 11) -- Belgium
+	(1, 22, 12), -- Israel
+	(1, 35, 13), -- Romania
+	(1, 6, 14), -- Azerbaijan
+	(1, 41, 15), -- Ukraine
+	(1, 27, 16), -- Malta
 
 -- 2nd Semifinal 20/5/2021
 insert into galaparticipant (gala_id, participant_id, performanceorder) values	
-	(2, 5, ), -- Austria
-	(2, 28, ), -- Moldova
-	(2, 31, ), -- Poland
-	(2, 37, ), -- San Marino 
-	(2, 38, ), -- Serbia
-	(2, 21, ), -- Iceland
-	(2, 34, ), -- Checz Republique
-	(2, 19, ), -- Greece
-	(2, 15, ), -- Estonia
-	(2, 12, ), -- Denmark
-	(2, 9, ), -- Bulgaria
-	(2, 40, ), -- Switzerland
-	(2, 16, ), -- Finland
-	(2, 3, ), -- Armenia
-	(2, 24, ), -- Latvia
-	(2, 18, ), -- Georgia
-	(2, 32, ), -- Portugal
-	(2, 1, ) -- Albania
+	(2, 37, 1), -- San Marino 
+	(2, 15, 2), -- Estonia
+	(2, 34, 3), -- Checz Republic
+	(2, 19, 4), -- Greece
+	(2, 5, 5), -- Austria
+	(2, 31, 6), -- Poland
+	(2, 28, 7), -- Moldova
+	(2, 21, 8), -- Iceland
+	(2, 38, 9), -- Serbia
+	(2, 18, 10), -- Georgia
+	(2, 1, 11) -- Albania
+	(2, 32, 12), -- Portugal
+	(2, 9, 13), -- Bulgaria
+	(2, 16, 14), -- Finland
+	(2, 24, 15), -- Latvia
+	(2, 40, 16), -- Switzerland
+	(2, 12, 17) -- Denmark
 
 -- Final
 insert into galaparticipant (gala_id, participant_id, performanceorder) values
