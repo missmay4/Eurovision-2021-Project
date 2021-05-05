edb898c25bbe7e9d45e2d7b8750a4f30ee9b3d60
Mayte <may.eater@gmail.com>
2021-05-05 21:41:01 +0200

    cors for api

--- node-eurovision/app.js
+++ node-eurovision/app.js
@@ -1,6 +1,10 @@
 // Express
 const express = require('express')
+var cors = require('cors')
 const server = express()
+
+//Middleware cors
+server.use(cors())
 
 // Database connection
 const connection_db = require('./db/conecction_db').connect
@@ -9,13 +13,19 @@ const connection_db = require('./db/conecction_db').connect
 const ParticipantRouter = require('./Routes/ParticipantRouter').router
 
 connection_db()
+
 // json use for data
 server.use(express.json())
 
 // route participant
 server.use('/', ParticipantRouter);
 
-server.listen(8080, () => {
-    console.log("Server starting on localhost:8080");
+// check if CORS is enabled for participants
+server.get('/participants', (request, response, next) => {
+    response.json({ msg: 'This is CORS-enabled for all origins!' })
+})
 
+server.listen(8080, () => {
+    console.log("Server starting on localhost:8080")
+    console.log('CORS-enabled web server listening on port 8080')
 });
